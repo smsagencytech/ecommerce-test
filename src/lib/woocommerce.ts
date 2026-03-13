5619227a2c80dc1af7dad770a66132f144d7183d
smsagencytech <smsagency.tech@gmail.com>
2026-03-13 18:13:21 +0000

    Fix: Rename NEXT_PUBLIC_WORDPRESS_URL to WORDPRESS_URL to satisfy Netlify secret scan

--- src/lib/woocommerce.ts
+++ src/lib/woocommerce.ts
@@ -1,7 +1,7 @@
 import WooCommerceRestApi from "@woocommerce/woocommerce-rest-api";
 
 const api = new WooCommerceRestApi({
-    url: process.env.NEXT_PUBLIC_WORDPRESS_URL || "",
+    url: process.env.WORDPRESS_URL || "",
     consumerKey: process.env.WOOCOMMERCE_KEY || "",
     consumerSecret: process.env.WOOCOMMERCE_SECRET || "",
     version: "wc/v3",
